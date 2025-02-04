dc0c4e80574a1474b468bb74022447f15a553883
Furkan Kozmaç <46871914+FurkanKozmac@users.noreply.github.com>
2025-02-05 00:22:46 +0300

    handled Validations with Zod and drizzle

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -6,13 +6,15 @@ import {
   deleteProducts,
   updateProducts,
 } from "./productsController";
+import { validateData } from "../../middlewares/validationMiddleware";
+import { createProductSchema, updateProductSchema } from "../../db/productsSchema";
 
 const router = Router();
 
 router.get("/", listProducts);
 router.get("/:id", getProductById);
-router.post("/", createProducts);
-router.put("/:id", updateProducts);
+router.post("/", validateData(createProductSchema), createProducts);
+router.put("/:id", validateData(updateProductSchema),updateProducts);
 router.delete("/:id", deleteProducts);
 
 export default router;
--- api/src/routes/products/productsController.ts
+++ api/src/routes/products/productsController.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import { db } from "../../db/index";
 import { productsTable } from "../../db/productsSchema";
 import { eq } from "drizzle-orm";
+import _ from "lodash";
+import { createProductSchema } from "../../db/productsSchema";
 
 export async function listProducts(req: Request, res: Response) {
   try {
@@ -34,7 +36,7 @@ export async function createProducts(req: Request, res: Response) {
   try {
     const [product] = await db
       .insert(productsTable)
-      .values(req.body)
+      .values(req.cleanBody)
       .returning();
     res.status(201).json(product);
   } catch (e) {
@@ -45,7 +47,7 @@ export async function createProducts(req: Request, res: Response) {
 export async function updateProducts(req: Request, res: Response) {
   try {
     const id = Number(req.params.id);
-    const updatedFields = req.body;
+    const updatedFields = req.cleanBody;
     const [product] = await db
       .update(productsTable)
       .set(updatedFields)
